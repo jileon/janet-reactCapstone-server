86314c97628029504df0358b3cee13a226e95763
Janet Leon <leon.irenejanet@gmail.com>
2018-10-15 12:59:36 -0700

    axios installed for api requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
 const morgan = require('morgan');
 
@@ -10,6 +11,8 @@ const { dbConnect } = require('./db-mongoose');
 
 const app = express();
 
+const GET_URL = 'https://newsapi.org/v2/top-headlines';
+const API_KEY = '722e175eefa74e1f9a66e5a9f5d86a76';
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -25,6 +28,18 @@ app.use(
 app.get('/api/newsflash', (req,res,next)=>{
   res.json('this is working');
 });
+
+
+app.get("/api/newsflash/everything", (request, response) => {
+ 
+  // add the API_KEY from the server side, no need to expose it on the client
+  const query = {params: {apiKey: API_KEY, country:'us'}};
+  axios.get(GET_URL, query)
+    .then(({data}) => response.json(data))
+    .catch(error => response.status(500).json(error));
+  
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
