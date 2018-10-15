c1ef5e91a41d54832e2f54d76dadf6b407c69021
Janet Leon <leon.irenejanet@gmail.com>
2018-10-15 15:11:37 -0700

    routes for different news categories

--- Routes/categoryRouter.js
+++ Routes/categoryRouter.js
@@ -25,4 +25,54 @@ router.get('/business', (request, response, next) => {
       
 });
 
+router.get('/health', (request, response, next) => {
+ 
+  //   add the API_KEY from the server side, no need to expose it on the client
+  const query = {params: {apiKey: API_KEY, country:'us', category:'health'}};
+  axios.get(GET_URL, query)
+    .then(({data}) => response.json(data))
+    .catch(error => response.status(500).json(error));
+      
+});
+
+router.get('/science', (request, response, next) => {
+ 
+  //   add the API_KEY from the server side, no need to expose it on the client
+  const query = {params: {apiKey: API_KEY, country:'us', category:'science'}};
+  axios.get(GET_URL, query)
+    .then(({data}) => response.json(data))
+    .catch(error => response.status(500).json(error));
+      
+});
+
+router.get('/sports', (request, response, next) => {
+ 
+  //   add the API_KEY from the server side, no need to expose it on the client
+  const query = {params: {apiKey: API_KEY, country:'us', category:'sports'}};
+  axios.get(GET_URL, query)
+    .then(({data}) => response.json(data))
+    .catch(error => response.status(500).json(error));
+      
+});
+
+router.get('/technology', (request, response, next) => {
+ 
+  //   add the API_KEY from the server side, no need to expose it on the client
+  const query = {params: {apiKey: API_KEY, country:'us', category:'technology'}};
+  axios.get(GET_URL, query)
+    .then(({data}) => response.json(data))
+    .catch(error => response.status(500).json(error));
+      
+});
+
+router.get('/entertainment', (request, response, next) => {
+ 
+  //   add the API_KEY from the server side, no need to expose it on the client
+  const query = {params: {apiKey: API_KEY, country:'us', category:'entertainment'}};
+  axios.get(GET_URL, query)
+    .then(({data}) => response.json(data))
+    .catch(error => response.status(500).json(error));
+      
+});
+
 module.exports =router;
