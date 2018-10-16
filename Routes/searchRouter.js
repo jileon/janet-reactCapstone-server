2e4339319111e5a694ce43dbe0ce88f9df3ccc62
Janet Leon <leon.irenejanet@gmail.com>
2018-10-15 22:32:27 -0700

    creating search query params

--- Routes/searchRouter.js
+++ Routes/searchRouter.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
-const GET_URL = 'https://newsapi.org/v2/top-headlines';
+const GET_URL = 'https://newsapi.org/v2/everything';
 const {API_KEY} = require('../config');
 
 router.get('/search', (request, response) => {
-//  const {searchTerm} = request.body;
- console.log(request.query);
+ const {searchTerm, page} = request.query;
+ console.log(searchTerm);
 
-  // add the API_KEY from the server side, no need to expose it on the client
-//   const query = {params: {apiKey: API_KEY, language:'en', q:searchTerm}};
-//   axios.get(GET_URL, query)
-//     .then(({data}) => response.json(data))
-//     .catch(error => response.status(500).json(error));
+ // add the API_KEY from the server side, no need to expose it on the client
+  const query = {params: {apiKey: API_KEY, language:'en', q:searchTerm, page:page}};
+  axios.get(GET_URL, query)
+    .then(({data}) => response.json(data))
+    .catch(error => response.status(500).json(error));
     
 });
 
