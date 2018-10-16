9101c80af273c7c1f9db50cc8d798f4d712f1501
Janet Leon <leon.irenejanet@gmail.com>
2018-10-16 13:50:02 -0700

    set up config for authentication

--- Routes/searchRouter.js
+++ Routes/searchRouter.js
@@ -5,10 +5,10 @@ const GET_URL = 'https://newsapi.org/v2/everything';
 const {API_KEY} = require('../config');
 
 router.get('/search', (request, response) => {
- const {searchTerm, page} = request.query;
- console.log(searchTerm);
+  const {searchTerm, page} = request.query;
+  console.log(searchTerm);
 
- // add the API_KEY from the server side, no need to expose it on the client
+  // add the API_KEY from the server side, no need to expose it on the client
   const query = {params: {apiKey: API_KEY, language:'en', q:searchTerm, page:page}};
   axios.get(GET_URL, query)
     .then(({data}) => response.json(data))
--- config.js
+++ config.js
@@ -8,10 +8,14 @@ module.exports = {
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/react-news-flash',
-  API_KEY : '722e175eefa74e1f9a66e5a9f5d86a76'
+  API_KEY : '722e175eefa74e1f9a66e5a9f5d86a76',
+  JWT_SECRET:process.env.JWT_SECRET,
+  JWT_EXPIRY:process.env.JWT_EXPIRY || '7d'
+
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
   //     process.env.TEST_DATABASE_URL ||
   //     'postgres://localhost/thinkful-backend-test'
 };
+
