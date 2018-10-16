9aba6cfe4d1c490a70edf07c59da6fcbb1d91548
Janet Leon <leon.irenejanet@gmail.com>
2018-10-15 17:01:58 -0700

    mounted router

--- Routes/categoryRouter.js
+++ Routes/categoryRouter.js
@@ -18,7 +18,7 @@ router.get('/everything', (request, response) => {
 router.get('/business', (request, response, next) => {
  
   //   add the API_KEY from the server side, no need to expose it on the client
-  const query = {params: {apiKey: API_KEY, country:'us', category:'business'}};
+  const query = {params: {apiKey: API_KEY, country:'en', category:'business'}};
   axios.get(GET_URL, query)
     .then(({data}) => response.json(data))
     .catch(error => response.status(500).json(error));
