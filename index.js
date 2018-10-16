82bfbf45a31d4ee98490b1c60ef8fdb5b6d2a805
Janet Leon <leon.irenejanet@gmail.com>
2018-10-16 14:31:36 -0700

    set up server for authentication

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict';
-
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-const { PORT, CLIENT_ORIGIN } = require('./config');
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+const { PORT, CLIENT_ORIGIN, DATABASE_URL  } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 const categoryRouter = require('./Routes/categoryRouter');
 const searchRouter = require('./Routes/searchRouter')
 const app = express();
 
+mongoose.Promise = global.Promise;
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -25,38 +30,79 @@ app.use(
   })
 );
 
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+
+
+// Mount routers
+app.use('/api/newsflash', categoryRouter);
+app.use('/api/newsflash', searchRouter);
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
+});
+
+
+
+
+
 //========Routes===================================
 app.get('/api/newsflash', (req,res,next)=>{
   res.json('this is working');
 });
 
-// Mount routers
-app.use('/api/newsflash', categoryRouter);
-app.use('/api/newsflash', searchRouter);
 
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
 
+// Referenced by both runServer and closeServer. closeServer
+// assumes runServer has run and set `server` to a server object
+let server;
 
+function runServer(databaseUrl, port = PORT) {
 
-
-
-
-
-
-
-function runServer(port = PORT) {
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
     });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
-module.exports = { app };
+module.exports = { app, runServer, closeServer };
+
