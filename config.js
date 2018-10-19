f627a2ae11f23413efc9772743c058c2731ecf34
Janet Leon <leon.irenejanet@gmail.com>
2018-10-19 10:36:21 -0700

    saved API key to process.env variable

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost:27017/test-react-news-flash',
-  API_KEY : '722e175eefa74e1f9a66e5a9f5d86a76',
+  API_KEY : process.env.API_KEY,
   JWT_SECRET:process.env.JWT_SECRET,
   JWT_EXPIRY:process.env.JWT_EXPIRY || '7d'
 
